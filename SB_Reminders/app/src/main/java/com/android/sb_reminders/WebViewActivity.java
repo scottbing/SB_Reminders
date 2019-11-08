be6bf96e2c5bdbbf9b0ddf25e19cd9afd5824a1b
scottbing <sbing52@comcast.net>
2019-11-08 14:03:46 -0700

    SB_Reminders
    SB_Reminders

--- SB_Reminders/app/src/main/java/com/android/sb_reminders/WebViewActivity.java
+++ SB_Reminders/app/src/main/java/com/android/sb_reminders/WebViewActivity.java
@@ -1,13 +1,18 @@
 package com.android.sb_reminders;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    private WebView webView;
     private String url;
 
     @Override
@@ -18,12 +23,52 @@ public class WebViewActivity extends AppCompatActivity {
         // get the passed data
         url = getIntent().getStringExtra("url");
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);     // recommended setting
 
         webView.loadUrl(url);
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            //
+            // Method onReceivedError() will show as deprecated without adding the code between the comment lines
+            // This code will handle onReceiveErroor() at various versions of the Adnroid SDK
+            // https://stackoverflow.com/questions/32769505/webviewclient-onreceivederror-deprecated-new-version-does-not-detect-all-errors
+            // 11/08/2019
+            //
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                // Handle the error
+                webView.loadUrl("file:///android_asset/error.html");
+            }
+
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+
+            //
+            // End of Annotated Code
+            //
+            //
+
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
     }
 
 }
